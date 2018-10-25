56520b5aa8102c914cf952df82f84af28f859b62
guochen <guochen@wealink.com>
2018-10-25 17:36:58 +0800

    up

--- src/main.c
+++ src/main.c
@@ -45,7 +45,7 @@ int main() {
     server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_address.sin_addr.s_addr = inet_addr("0.0.0.0");
     server_address.sin_port = htons(9734);
     server_len = sizeof(server_address);
     bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
